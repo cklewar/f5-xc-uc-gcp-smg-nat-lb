25d1be5a758def675a143c961593af488471088e
cklewar <cklewar@t-online.de>
2023-01-25 01:32:38 +0100

    0.11.16 v2

--- versions.tf
+++ versions.tf
@@ -15,6 +15,11 @@ terraform {
       version = "= 0.11.16"
     }
 
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.39.0"
+    }
+
     local = ">= 2.2.3"
     null  = ">= 3.1.1"
   }
