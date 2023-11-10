45973a2401228f6e847706f7c7f935cad4d0210e
cklewar <cklewar@t-online.de>
2023-11-10 15:15:24 +0100

    Launch 0.11.23

--- versions.tf
+++ versions.tf
@@ -12,7 +12,7 @@ terraform {
   required_providers {
     volterra = {
       source = "volterraedge/volterra"
-      version = "= 0.11.18"
+      version = "= 0.11.23"
     }
     google = {
       source = "hashicorp/google"
